ac75abfb74eb57515cd4d87cc33c4a0aa09cdba4
Alberto Romo <calmf22@live.com>
2015-11-13 08:34:27 -0800

    primer commit

---
+++ BD/EF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nexus2.BD
+{
+    class EF:DbContext
+    {
+       //aqui van las tablas que se usaran
+    }
+}
